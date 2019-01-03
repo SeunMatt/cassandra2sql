ad59317306dab1308af95580620d53bd5480866a
Seun Matt <smatt382@gmail.com>
2019-01-03 09:42:53 +0100

    updated the db prep file and test classes

--- src/test/java/com/smattme/cassandra2sql/DatabaseServiceIntegrationTest.java
+++ src/test/java/com/smattme/cassandra2sql/DatabaseServiceIntegrationTest.java
@@ -1,8 +1,6 @@
 package com.smattme.cassandra2sql;
 
-import com.smattme.cassandra2sql.services.ConnectionService;
 import com.smattme.cassandra2sql.services.DatabaseService;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +9,7 @@ import java.io.File;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 class DatabaseServiceIntegrationTest {
 
@@ -19,8 +18,10 @@ class DatabaseServiceIntegrationTest {
 
     @Test
     void givenPropertyFilePath_whenGenerateSQLFromKeySpace_thenReadConfig() {
-
-        DatabaseService databaseService = new DatabaseService("application.properties");
+        String propertyFile = "application.properties";
+        File file = new File(propertyFile);
+        assumeTrue(file.exists(), propertyFile + " does not exist so test is skipped");
+        DatabaseService databaseService = new DatabaseService(propertyFile);
         databaseService.generateSQLFromKeySpace();
         Map<String, String> generatedSQL = databaseService.getGeneratedSQL();
         File generatedZipFile = databaseService.getGeneratedZipFile();
@@ -28,14 +29,17 @@ class DatabaseServiceIntegrationTest {
         assertNotNull(generatedSQL);
         assertNotNull(generatedZipFile);
 
-//        logger.info("generated zip file: " + generatedZipFile.getAbsolutePath());
-//        logger.info("generated sql: " + generatedSQL);
+        logger.info("generated zip file: \n" + generatedZipFile.getAbsolutePath());
+        logger.info("generated sql: \n" + generatedSQL);
     }
 
 
     @Test
     void givenAlternatePropertyFilePath_whenGenerateSQL_thenDoNotPreserveGeneratedFiles() {
-        DatabaseService databaseService = new DatabaseService("application-no-preserve.properties");
+        String propertyFile = "application-no-preserve.properties";
+        File file = new File(propertyFile);
+        assumeTrue(file.exists(), propertyFile + " does not exist so test is skipped");
+        DatabaseService databaseService = new DatabaseService(propertyFile);
         databaseService.generateSQLFromKeySpace();
         Map<String, String> generatedSQL = databaseService.getGeneratedSQL();
         File generatedZipFile = databaseService.getGeneratedZipFile();
@@ -46,6 +50,9 @@ class DatabaseServiceIntegrationTest {
 
     @Test
     void givenPropertyFile_whenGenerateSQL_thenDoNotSendEmail() {
+        String propertyFile = "application-no-email.properties";
+        File file = new File(propertyFile);
+        assumeTrue(file.exists(), propertyFile + " does not exist so test is skipped");
         DatabaseService databaseService = new DatabaseService("application-no-email.properties");
         databaseService.generateSQLFromKeySpace();
         Map<String, String> generatedSQL = databaseService.getGeneratedSQL();
